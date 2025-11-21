590cce4bc7109246daaf7c92c2de2ca21151578c
Abhishek Kumar <144677705+Purbey2001@users.noreply.github.com>
2025-11-21 19:12:38 +0530

    added render url

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // backend URL
+  baseURL: "https://tinyurl-67q8.onrender.com", // backend URL
 });
 
 export default API;
